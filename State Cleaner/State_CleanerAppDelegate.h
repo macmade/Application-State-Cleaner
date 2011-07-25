cda1c878c77fa7b11ad25e27e83e29eec81164f9
Jean-David Gadina <macmade@eosgarden.com>
2011-07-25 14:14:03 +0200

    Initial Commit

---
+++ State Cleaner/State_CleanerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  State_CleanerAppDelegate.h
+//  State Cleaner
+//
+//  Created by Jean-David Gadina on 25.07.2011.
+//  Copyright 2011 DigiDNA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface State_CleanerAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ State Cleaner/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  State Cleaner
+//
+//  Created by Jean-David Gadina on 25.07.2011.
+//  Copyright 2011 DigiDNA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
